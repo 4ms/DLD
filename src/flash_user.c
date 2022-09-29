55c2f17567e85c8dae667103ef5d82d58923b8c7
Dan Green <danngreen1@gmail.com>
2022-09-29 09:48:00 -0700

    formatting

--- src/flash_user.c
+++ src/flash_user.c
@@ -26,20 +26,20 @@
  * -----------------------------------------------------------------------------
  */
 
-#include "globals.h"
-#include "flash.h"
 #include "flash_user.h"
-#include "calibration.h"
-#include "system_settings.h"
-#include "params.h"
 #include "adc.h"
-#include "leds.h"
+#include "calibration.h"
 #include "dig_pins.h"
+#include "flash.h"
+#include "flash_layout.h"
+#include "globals.h"
+#include "leds.h"
+#include "params.h"
+#include "system_settings.h"
 
 extern int16_t CV_CALIBRATION_OFFSET[6];
 extern int16_t CODEC_DAC_CALIBRATION_DCOFFSET[4];
 extern int16_t CODEC_ADC_CALIBRATION_DCOFFSET[4];
-
 
 extern float param[NUM_CHAN][NUM_PARAMS];
 extern uint8_t mode[NUM_CHAN][NUM_CHAN_MODES];
@@ -48,87 +48,86 @@ extern float global_param[NUM_GLOBAL_PARAMS];
 
 extern int16_t i_smoothed_cvadc[NUM_POT_ADCS];
 
+// TODO: use a struct!!
 
-#define FLASH_ADDR_userparams 0x08004000
 //Globals:
-#define FLASH_ADDR_firmware_version			FLASH_ADDR_userparams									/* 0  ..3	*/
+#define FLASH_ADDR_firmware_version FLASH_ADDR_userparams /* 0  ..3	*/
 #define SZ_FV 4
 
-#define FLASH_ADDR_cv_calibration_offset	(FLASH_ADDR_firmware_version			+ SZ_FV)		/* 4  ..27	*/
+#define FLASH_ADDR_cv_calibration_offset (FLASH_ADDR_firmware_version + SZ_FV) /* 4  ..27	*/
 #define SZ_CVCAL 24
 
-#define FLASH_ADDR_adc_calibration_dcoffset	(FLASH_ADDR_cv_calibration_offset 		+ SZ_CVCAL)		/* 28  ..43	*/
+#define FLASH_ADDR_adc_calibration_dcoffset (FLASH_ADDR_cv_calibration_offset + SZ_CVCAL) /* 28  ..43	*/
 #define SZ_ADCCAL 16
 
-#define FLASH_ADDR_dac_calibration_dcoffset	(FLASH_ADDR_adc_calibration_dcoffset 	+ SZ_ADCCAL)	/* 44  ..59	*/
+#define FLASH_ADDR_dac_calibration_dcoffset (FLASH_ADDR_adc_calibration_dcoffset + SZ_ADCCAL) /* 44  ..59	*/
 #define SZ_DACCAL 16
 
-#define FLASH_ADDR_TRACKING_COMP_0			(FLASH_ADDR_dac_calibration_dcoffset 	+ SZ_DACCAL)	/* 60  ..63	*/
+#define FLASH_ADDR_TRACKING_COMP_0 (FLASH_ADDR_dac_calibration_dcoffset + SZ_DACCAL) /* 60  ..63	*/
 #define SZ_TC0 4
 
-#define FLASH_ADDR_TRACKING_COMP_1			(FLASH_ADDR_TRACKING_COMP_0 			+ SZ_TC0)		/* 64  ..67	*/
+#define FLASH_ADDR_TRACKING_COMP_1 (FLASH_ADDR_TRACKING_COMP_0 + SZ_TC0) /* 64  ..67	*/
 #define SZ_TC1 4
 
-#define FLASH_ADDR_loop_led_brightness		(FLASH_ADDR_TRACKING_COMP_1 			+ SZ_TC1)		/* 68  ..	*/
+#define FLASH_ADDR_loop_led_brightness (FLASH_ADDR_TRACKING_COMP_1 + SZ_TC1) /* 68  ..	*/
 #define SZ_LLB 1
 
-#define FLASH_ADDR_LOOP_CLOCK_GATETRIG_0	(FLASH_ADDR_loop_led_brightness 		+ SZ_LLB)		/* 69  ..	*/
+#define FLASH_ADDR_LOOP_CLOCK_GATETRIG_0 (FLASH_ADDR_loop_led_brightness + SZ_LLB) /* 69  ..	*/
 #define SZ_LC0 1
 
-#define FLASH_ADDR_LOOP_CLOCK_GATETRIG_1	(FLASH_ADDR_LOOP_CLOCK_GATETRIG_0 		+ SZ_LC0)		/* 70  ..	*/
+#define FLASH_ADDR_LOOP_CLOCK_GATETRIG_1 (FLASH_ADDR_LOOP_CLOCK_GATETRIG_0 + SZ_LC0) /* 70  ..	*/
 #define SZ_LC1 1
 
-#define FLASH_ADDR_MAIN_CLOCK_GATETRIG		(FLASH_ADDR_LOOP_CLOCK_GATETRIG_1 		+ SZ_LC1)		/* 71  ..	*/
-#define SZ_MC  1
+#define FLASH_ADDR_MAIN_CLOCK_GATETRIG (FLASH_ADDR_LOOP_CLOCK_GATETRIG_1 + SZ_LC1) /* 71  ..	*/
+#define SZ_MC 1
 
-#define FLASH_ADDR_AUTO_MUTE				(FLASH_ADDR_MAIN_CLOCK_GATETRIG 		+ SZ_MC)		/* 72  ..	*/
+#define FLASH_ADDR_AUTO_MUTE (FLASH_ADDR_MAIN_CLOCK_GATETRIG + SZ_MC) /* 72  ..	*/
 #define SZ_AM 1
 
-#define FLASH_ADDR_SOFTCLIP					(FLASH_ADDR_AUTO_MUTE			 		+ SZ_AM)		/* 73  ..	*/
+#define FLASH_ADDR_SOFTCLIP (FLASH_ADDR_AUTO_MUTE + SZ_AM) /* 73  ..	*/
 #define SZ_SC 1
 
-#define FLASH_ADDR_LEVELCV_IS_MIX_0			(FLASH_ADDR_SOFTCLIP			 		+ SZ_SC)		/* 74  ..	*/
+#define FLASH_ADDR_LEVELCV_IS_MIX_0 (FLASH_ADDR_SOFTCLIP + SZ_SC) /* 74  ..	*/
 #define SZ_LM0 1
 
-#define FLASH_ADDR_LEVELCV_IS_MIX_1			(FLASH_ADDR_LEVELCV_IS_MIX_0	 		+ SZ_LM0)		/* 75  ..	*/
+#define FLASH_ADDR_LEVELCV_IS_MIX_1 (FLASH_ADDR_LEVELCV_IS_MIX_0 + SZ_LM0) /* 75  ..	*/
 #define SZ_LM1 1
 
-#define FLASH_ADDR_FAST_FADE_SAMPLES		(FLASH_ADDR_LEVELCV_IS_MIX_1	 		+ SZ_LM1)		/* 76  ..79	*/
+#define FLASH_ADDR_FAST_FADE_SAMPLES (FLASH_ADDR_LEVELCV_IS_MIX_1 + SZ_LM1) /* 76  ..79	*/
 #define SZ_FFS 4
 
-#define FLASH_ADDR_SLOW_FADE_SAMPLES		(FLASH_ADDR_FAST_FADE_SAMPLES	 		+ SZ_FFS)		/* 80  ..83	*/
+#define FLASH_ADDR_SLOW_FADE_SAMPLES (FLASH_ADDR_FAST_FADE_SAMPLES + SZ_FFS) /* 80  ..83	*/
 #define SZ_SFS 4
 
-#define FLASH_ADDR_REV_GATETRIG				(FLASH_ADDR_SLOW_FADE_SAMPLES			+ SZ_SFS)		/* 84 ..	*/
+#define FLASH_ADDR_REV_GATETRIG (FLASH_ADDR_SLOW_FADE_SAMPLES + SZ_SFS) /* 84 ..	*/
 #define SZ_RGT 1
 
-#define FLASH_ADDR_INF_GATETRIG				(FLASH_ADDR_REV_GATETRIG				+ SZ_RGT)		/* 85 ..	*/
+#define FLASH_ADDR_INF_GATETRIG (FLASH_ADDR_REV_GATETRIG + SZ_RGT) /* 85 ..	*/
 #define SZ_IGT 1
 
-#define FLASH_ADDR_PING_METHOD				(FLASH_ADDR_INF_GATETRIG				+ SZ_IGT)		/* 86 ..	*/
+#define FLASH_ADDR_PING_METHOD (FLASH_ADDR_INF_GATETRIG + SZ_IGT) /* 86 ..	*/
 #define SZ_PM 1
 
-#define FLASH_ADDR_LOG_DELAY_FEED				(FLASH_ADDR_PING_METHOD				+ SZ_PM)		/* 87 ..	*/
+#define FLASH_ADDR_LOG_DELAY_FEED (FLASH_ADDR_PING_METHOD + SZ_PM) /* 87 ..	*/
 #define SZ_LDF 1
 
-#define FLASH_ADDR_RUNAWAYDC_BLOCK				(FLASH_ADDR_LOG_DELAY_FEED			+ SZ_LDF)		/* 88 ..	*/
+#define FLASH_ADDR_RUNAWAYDC_BLOCK (FLASH_ADDR_LOG_DELAY_FEED + SZ_LDF) /* 88 ..	*/
 #define SZ_RDCB 1
 
-#define FLASH_ADDR_QUANTIZE_MODE_CHANGES				(FLASH_ADDR_RUNAWAYDC_BLOCK		+ SZ_RDCB)		/* 89 ..	*/
+#define FLASH_ADDR_QUANTIZE_MODE_CHANGES (FLASH_ADDR_RUNAWAYDC_BLOCK + SZ_RDCB) /* 89 ..	*/
 #define SZ_QCM 1
 
-#define FLASH_ADDR_SEND_RETURN_BEFORE_LOOP_0				(FLASH_ADDR_QUANTIZE_MODE_CHANGES		+ SZ_QCM)		/* 90 ..	*/
+#define FLASH_ADDR_SEND_RETURN_BEFORE_LOOP_0 (FLASH_ADDR_QUANTIZE_MODE_CHANGES + SZ_QCM) /* 90 ..	*/
 #define SZ_SRBL0 1
 
-#define FLASH_ADDR_SEND_RETURN_BEFORE_LOOP_1				(FLASH_ADDR_SEND_RETURN_BEFORE_LOOP_0		+ SZ_SRBL0)		/* 91 ..	*/
+#define FLASH_ADDR_SEND_RETURN_BEFORE_LOOP_1 (FLASH_ADDR_SEND_RETURN_BEFORE_LOOP_0 + SZ_SRBL0) /* 91 ..	*/
 #define SZ_SRBL1 1
-
 
 #define FLASH_SYMBOL_bankfilled 0x01
 #define FLASH_SYMBOL_startupoffset 0xAA
 #define FLASH_SYMBOL_firmwareoffset 0xAA550000
 
-uint32_t flash_firmware_version=0;
+uint32_t flash_firmware_version = 0;
 
 int32_t flash_CV_CALIBRATION_OFFSET[6];
 int32_t flash_CODEC_DAC_CALIBRATION_DCOFFSET[4];
@@ -163,16 +162,12 @@ uint8_t flash_global_mode_QUANTIZE_MODE_CHANGES;
 uint8_t flash_mode_SEND_RETURN_BEFORE_LOOP_0;
 uint8_t flash_mode_SEND_RETURN_BEFORE_LOOP_1;
 
-
-void set_firmware_version(void)
-{
+void set_firmware_version(void) {
 	flash_firmware_version = FW_VERSION;
 }
 
-
-void factory_reset(uint8_t loop_afterwards)
-{
-	uint8_t i,fail=0;
+void factory_reset(uint8_t loop_afterwards) {
+	uint8_t i, fail = 0;
 
 	auto_calibrate();
 	set_firmware_version();
@@ -197,20 +192,18 @@ void factory_reset(uint8_t loop_afterwards)
 	LED_INF1_OFF;
 	LED_INF2_OFF;
 
-	if (loop_afterwards)
-	{
-		fail=0;
-		for (i=0;i<6;i++)
-		{
-			if (CV_CALIBRATION_OFFSET[i]>100 || CV_CALIBRATION_OFFSET[i]<-100 )
-				fail=1;
+	if (loop_afterwards) {
+		fail = 0;
+		for (i = 0; i < 6; i++) {
+			if (CV_CALIBRATION_OFFSET[i] > 100 || CV_CALIBRATION_OFFSET[i] < -100)
+				fail = 1;
 		}
 
-		if (i_smoothed_cvadc[0] > 2150 || i_smoothed_cvadc[0]<1950 || i_smoothed_cvadc[1] > 2150 || i_smoothed_cvadc[1]<1950)
-			fail=1;
+		if (i_smoothed_cvadc[0] > 2150 || i_smoothed_cvadc[0] < 1950 || i_smoothed_cvadc[1] > 2150 ||
+			i_smoothed_cvadc[1] < 1950)
+			fail = 1;
 
-		while(1)
-		{
+		while (1) {
 			if (fail)
 				blink_all_lights(200); //error: did not auto-calibrate!
 			else
@@ -219,12 +212,11 @@ void factory_reset(uint8_t loop_afterwards)
 	}
 }
 
-uint32_t load_flash_params(void)
-{
+uint32_t load_flash_params(void) {
 
 	read_all_params_from_FLASH();
 
-	if (flash_firmware_version > 0 && flash_firmware_version < 500){
+	if (flash_firmware_version > 0 && flash_firmware_version < 500) {
 
 		CODEC_ADC_CALIBRATION_DCOFFSET[0] = flash_CODEC_ADC_CALIBRATION_DCOFFSET[0];
 		CODEC_ADC_CALIBRATION_DCOFFSET[1] = flash_CODEC_ADC_CALIBRATION_DCOFFSET[1];
@@ -263,31 +255,27 @@ uint32_t load_flash_params(void)
 		global_mode[RUNAWAYDC_BLOCK] = flash_global_mode_RUNAWAYDC_BLOCK;
 		global_mode[QUANTIZE_MODE_CHANGES] = flash_global_mode_QUANTIZE_MODE_CHANGES;
 
-
-		mode[0][LEVELCV_IS_MIX] = (flash_mode_LEVELCV_IS_MIX_0==1) ? 1:0;
-		mode[1][LEVELCV_IS_MIX] = (flash_mode_LEVELCV_IS_MIX_1==1) ? 1:0;
+		mode[0][LEVELCV_IS_MIX] = (flash_mode_LEVELCV_IS_MIX_0 == 1) ? 1 : 0;
+		mode[1][LEVELCV_IS_MIX] = (flash_mode_LEVELCV_IS_MIX_1 == 1) ? 1 : 0;
 
 		global_param[FAST_FADE_SAMPLES] = (float)flash_global_param_FAST_FADE_SAMPLES;
 		global_param[SLOW_FADE_SAMPLES] = (float)flash_global_param_SLOW_FADE_SAMPLES;
 		global_param[FAST_FADE_INCREMENT] = set_fade_increment(flash_global_param_FAST_FADE_SAMPLES);
 		global_param[SLOW_FADE_INCREMENT] = set_fade_increment(flash_global_param_SLOW_FADE_SAMPLES);
 
-		mode[0][SEND_RETURN_BEFORE_LOOP] = (flash_mode_SEND_RETURN_BEFORE_LOOP_0==1) ? 1:0;
-		mode[1][SEND_RETURN_BEFORE_LOOP] = (flash_mode_SEND_RETURN_BEFORE_LOOP_1==1) ? 1:0;
+		mode[0][SEND_RETURN_BEFORE_LOOP] = (flash_mode_SEND_RETURN_BEFORE_LOOP_0 == 1) ? 1 : 0;
+		mode[1][SEND_RETURN_BEFORE_LOOP] = (flash_mode_SEND_RETURN_BEFORE_LOOP_1 == 1) ? 1 : 0;
 
 		return (flash_firmware_version);
 
-	} else
-	{
-    	set_firmware_version();
+	} else {
+		set_firmware_version();
 
-		return(0); //No calibration data found
+		return (0); //No calibration data found
 	}
 }
 
-
-void save_flash_params(void)
-{
+void save_flash_params(void) {
 	uint32_t i;
 
 	LED_REV1_ON;
@@ -303,7 +291,7 @@ void save_flash_params(void)
 	//copy SRAM variables to FLASH
 	write_all_params_to_FLASH();
 
-	for (i=0;i<10;i++){
+	for (i = 0; i < 10; i++) {
 		LED_PINGBUT_OFF;
 		LED_REV1_OFF;
 		LED_REV2_OFF;
@@ -324,26 +312,23 @@ void save_flash_params(void)
 	}
 }
 
+void store_params_into_sram(void) {
+	flash_CV_CALIBRATION_OFFSET[0] = CV_CALIBRATION_OFFSET[0];
+	flash_CV_CALIBRATION_OFFSET[1] = CV_CALIBRATION_OFFSET[1];
+	flash_CV_CALIBRATION_OFFSET[2] = CV_CALIBRATION_OFFSET[2];
+	flash_CV_CALIBRATION_OFFSET[3] = CV_CALIBRATION_OFFSET[3];
+	flash_CV_CALIBRATION_OFFSET[4] = CV_CALIBRATION_OFFSET[4];
+	flash_CV_CALIBRATION_OFFSET[5] = CV_CALIBRATION_OFFSET[5];
 
-void store_params_into_sram(void)
-{
-	flash_CV_CALIBRATION_OFFSET[0]=CV_CALIBRATION_OFFSET[0];
-	flash_CV_CALIBRATION_OFFSET[1]=CV_CALIBRATION_OFFSET[1];
-	flash_CV_CALIBRATION_OFFSET[2]=CV_CALIBRATION_OFFSET[2];
-	flash_CV_CALIBRATION_OFFSET[3]=CV_CALIBRATION_OFFSET[3];
-	flash_CV_CALIBRATION_OFFSET[4]=CV_CALIBRATION_OFFSET[4];
-	flash_CV_CALIBRATION_OFFSET[5]=CV_CALIBRATION_OFFSET[5];
+	flash_CODEC_DAC_CALIBRATION_DCOFFSET[0] = CODEC_DAC_CALIBRATION_DCOFFSET[0];
+	flash_CODEC_DAC_CALIBRATION_DCOFFSET[1] = CODEC_DAC_CALIBRATION_DCOFFSET[1];
+	flash_CODEC_DAC_CALIBRATION_DCOFFSET[2] = CODEC_DAC_CALIBRATION_DCOFFSET[2];
+	flash_CODEC_DAC_CALIBRATION_DCOFFSET[3] = CODEC_DAC_CALIBRATION_DCOFFSET[3];
 
-	flash_CODEC_DAC_CALIBRATION_DCOFFSET[0]=CODEC_DAC_CALIBRATION_DCOFFSET[0];
-	flash_CODEC_DAC_CALIBRATION_DCOFFSET[1]=CODEC_DAC_CALIBRATION_DCOFFSET[1];
-	flash_CODEC_DAC_CALIBRATION_DCOFFSET[2]=CODEC_DAC_CALIBRATION_DCOFFSET[2];
-	flash_CODEC_DAC_CALIBRATION_DCOFFSET[3]=CODEC_DAC_CALIBRATION_DCOFFSET[3];
-
-
-	flash_CODEC_ADC_CALIBRATION_DCOFFSET[0]=CODEC_ADC_CALIBRATION_DCOFFSET[0];
-	flash_CODEC_ADC_CALIBRATION_DCOFFSET[1]=CODEC_ADC_CALIBRATION_DCOFFSET[1];
-	flash_CODEC_ADC_CALIBRATION_DCOFFSET[2]=CODEC_ADC_CALIBRATION_DCOFFSET[2];
-	flash_CODEC_ADC_CALIBRATION_DCOFFSET[3]=CODEC_ADC_CALIBRATION_DCOFFSET[3];
+	flash_CODEC_ADC_CALIBRATION_DCOFFSET[0] = CODEC_ADC_CALIBRATION_DCOFFSET[0];
+	flash_CODEC_ADC_CALIBRATION_DCOFFSET[1] = CODEC_ADC_CALIBRATION_DCOFFSET[1];
+	flash_CODEC_ADC_CALIBRATION_DCOFFSET[2] = CODEC_ADC_CALIBRATION_DCOFFSET[2];
+	flash_CODEC_ADC_CALIBRATION_DCOFFSET[3] = CODEC_ADC_CALIBRATION_DCOFFSET[3];
 
 	flash_param_TRACKING_COMP_0 = param[0][TRACKING_COMP];
 	flash_param_TRACKING_COMP_1 = param[1][TRACKING_COMP];
@@ -376,9 +361,7 @@ void store_params_into_sram(void)
 	flash_mode_SEND_RETURN_BEFORE_LOOP_1 = mode[1][SEND_RETURN_BEFORE_LOOP];
 }
 
-
-void write_all_params_to_FLASH(void)
-{
+void write_all_params_to_FLASH(void) {
 	uint8_t i;
 
 	flash_begin_open_program();
@@ -387,18 +370,16 @@ void write_all_params_to_FLASH(void)
 
 	flash_open_program_word(flash_firmware_version + FLASH_SYMBOL_firmwareoffset, FLASH_ADDR_firmware_version);
 
-
-	for (i=0;i<6;i++)
-	{
-		flash_open_program_word(*(uint32_t *)&(flash_CV_CALIBRATION_OFFSET[i]), FLASH_ADDR_cv_calibration_offset+(i*4));
+	for (i = 0; i < 6; i++) {
+		flash_open_program_word(*(uint32_t *)&(flash_CV_CALIBRATION_OFFSET[i]),
+								FLASH_ADDR_cv_calibration_offset + (i * 4));
 	}
-	for (i=0;i<4;i++)
-	{
-		flash_open_program_word(*(uint32_t *)&(flash_CODEC_DAC_CALIBRATION_DCOFFSET[i]), FLASH_ADDR_dac_calibration_dcoffset+(i*4));
-		flash_open_program_word(*(uint32_t *)&(flash_CODEC_ADC_CALIBRATION_DCOFFSET[i]), FLASH_ADDR_adc_calibration_dcoffset+(i*4));
+	for (i = 0; i < 4; i++) {
+		flash_open_program_word(*(uint32_t *)&(flash_CODEC_DAC_CALIBRATION_DCOFFSET[i]),
+								FLASH_ADDR_dac_calibration_dcoffset + (i * 4));
+		flash_open_program_word(*(uint32_t *)&(flash_CODEC_ADC_CALIBRATION_DCOFFSET[i]),
+								FLASH_ADDR_adc_calibration_dcoffset + (i * 4));
 	}
-
-
 
 	flash_open_program_word(*(uint32_t *)&flash_param_TRACKING_COMP_0, FLASH_ADDR_TRACKING_COMP_0);
 	flash_open_program_word(*(uint32_t *)&flash_param_TRACKING_COMP_1, FLASH_ADDR_TRACKING_COMP_1);
@@ -432,31 +413,26 @@ void write_all_params_to_FLASH(void)
 	flash_end_open_program();
 }
 
-
-void read_all_params_from_FLASH(void)
-{
+void read_all_params_from_FLASH(void) {
 	uint8_t i;
 
 	flash_firmware_version = flash_read_word(FLASH_ADDR_firmware_version) - FLASH_SYMBOL_firmwareoffset;
 
-	for (i=0;i<6;i++)
-	{
-		flash_CV_CALIBRATION_OFFSET[i] = flash_read_word(FLASH_ADDR_cv_calibration_offset+(i*4));
+	for (i = 0; i < 6; i++) {
+		flash_CV_CALIBRATION_OFFSET[i] = flash_read_word(FLASH_ADDR_cv_calibration_offset + (i * 4));
 	}
-	for (i=0;i<4;i++)
-	{
-		flash_CODEC_DAC_CALIBRATION_DCOFFSET[i] = flash_read_word(FLASH_ADDR_dac_calibration_dcoffset+(i*4));
-		flash_CODEC_ADC_CALIBRATION_DCOFFSET[i] = flash_read_word(FLASH_ADDR_adc_calibration_dcoffset+(i*4));
+	for (i = 0; i < 4; i++) {
+		flash_CODEC_DAC_CALIBRATION_DCOFFSET[i] = flash_read_word(FLASH_ADDR_dac_calibration_dcoffset + (i * 4));
+		flash_CODEC_ADC_CALIBRATION_DCOFFSET[i] = flash_read_word(FLASH_ADDR_adc_calibration_dcoffset + (i * 4));
 	}
 
 	flash_param_TRACKING_COMP_0 = flash_read_word(FLASH_ADDR_TRACKING_COMP_0);
-	if (flash_param_TRACKING_COMP_0 > 1.25 || flash_param_TRACKING_COMP_0 < 0.75)
-		flash_param_TRACKING_COMP_0 = 1.0;
+	if (flash_param_TRACKING_COMP_0 > 1.25f || flash_param_TRACKING_COMP_0 < 0.75f)
+		flash_param_TRACKING_COMP_0 = 1.0f;
 
 	flash_param_TRACKING_COMP_1 = flash_read_word(FLASH_ADDR_TRACKING_COMP_1);
-	if (flash_param_TRACKING_COMP_1 > 1.25 || flash_param_TRACKING_COMP_1 < 0.75)
-		flash_param_TRACKING_COMP_1 = 1.0;
-
+	if (flash_param_TRACKING_COMP_1 > 1.25f || flash_param_TRACKING_COMP_1 < 0.75f)
+		flash_param_TRACKING_COMP_1 = 1.0f;
 
 	flash_global_param_FAST_FADE_SAMPLES = flash_read_word(FLASH_ADDR_FAST_FADE_SAMPLES);
 	if (flash_global_param_FAST_FADE_SAMPLES < 1 || flash_global_param_FAST_FADE_SAMPLES > 48000)
@@ -472,29 +448,30 @@ void read_all_params_from_FLASH(void)
 
 	flash_mode_LOOP_CLOCK_GATETRIG_0 = flash_read_byte(FLASH_ADDR_LOOP_CLOCK_GATETRIG_0) ? 1 : 0; //default trig
 	flash_mode_LOOP_CLOCK_GATETRIG_1 = flash_read_byte(FLASH_ADDR_LOOP_CLOCK_GATETRIG_1) ? 1 : 0; //default trig
-	flash_mode_MAIN_CLOCK_GATETRIG = flash_read_byte(FLASH_ADDR_MAIN_CLOCK_GATETRIG) ? 1 : 0; //default trig
+	flash_mode_MAIN_CLOCK_GATETRIG = flash_read_byte(FLASH_ADDR_MAIN_CLOCK_GATETRIG) ? 1 : 0;	  //default trig
 
-	flash_global_mode_AUTO_MUTE = flash_read_byte(FLASH_ADDR_AUTO_MUTE) ? 1 : 0;  //default ON
-	flash_global_mode_SOFTCLIP = flash_read_byte(FLASH_ADDR_SOFTCLIP) ? 1 : 0; //default ON
+	flash_global_mode_AUTO_MUTE = flash_read_byte(FLASH_ADDR_AUTO_MUTE) ? 1 : 0; //default ON
+	flash_global_mode_SOFTCLIP = flash_read_byte(FLASH_ADDR_SOFTCLIP) ? 1 : 0;	 //default ON
 
-
-	flash_mode_LEVELCV_IS_MIX_0 = (flash_read_byte(FLASH_ADDR_LEVELCV_IS_MIX_0)==1) ? 1 : 0; //default OFF
-	flash_mode_LEVELCV_IS_MIX_1 = (flash_read_byte(FLASH_ADDR_LEVELCV_IS_MIX_1)==1) ? 1 : 0; //default OFF
+	flash_mode_LEVELCV_IS_MIX_0 = (flash_read_byte(FLASH_ADDR_LEVELCV_IS_MIX_0) == 1) ? 1 : 0; //default OFF
+	flash_mode_LEVELCV_IS_MIX_1 = (flash_read_byte(FLASH_ADDR_LEVELCV_IS_MIX_1) == 1) ? 1 : 0; //default OFF
 
 	flash_global_mode_PING_METHOD = flash_read_byte(FLASH_ADDR_PING_METHOD);
 	if (flash_global_mode_PING_METHOD > NUM_PING_METHODS || flash_global_mode_PING_METHOD < 0)
 		flash_global_mode_PING_METHOD = IGNORE_FLAT_DEVIATION_10;
 
-	flash_global_mode_LOG_DELAY_FEED = (flash_read_byte(FLASH_ADDR_LOG_DELAY_FEED)==1) ? 1 : 0; //default OFF
+	flash_global_mode_LOG_DELAY_FEED = (flash_read_byte(FLASH_ADDR_LOG_DELAY_FEED) == 1) ? 1 : 0; //default OFF
 
-	flash_global_mode_RUNAWAYDC_BLOCK = (flash_read_byte(FLASH_ADDR_RUNAWAYDC_BLOCK)==0) ? 0 : 1; //default ON
+	flash_global_mode_RUNAWAYDC_BLOCK = (flash_read_byte(FLASH_ADDR_RUNAWAYDC_BLOCK) == 0) ? 0 : 1; //default ON
 
-	flash_global_mode_QUANTIZE_MODE_CHANGES = (flash_read_byte(FLASH_ADDR_QUANTIZE_MODE_CHANGES)==1) ? 1 : 0; //default OFF
+	flash_global_mode_QUANTIZE_MODE_CHANGES =
+		(flash_read_byte(FLASH_ADDR_QUANTIZE_MODE_CHANGES) == 1) ? 1 : 0; //default OFF
 
 	flash_global_mode_REV_GATETRIG = flash_read_byte(FLASH_ADDR_REV_GATETRIG) ? 1 : 0; //default trig
 	flash_global_mode_INF_GATETRIG = flash_read_byte(FLASH_ADDR_INF_GATETRIG) ? 1 : 0; //default trig
 
-	flash_mode_SEND_RETURN_BEFORE_LOOP_0 = (flash_read_byte(FLASH_ADDR_SEND_RETURN_BEFORE_LOOP_0)==1) ? 1 : 0; //default OFF
-	flash_mode_SEND_RETURN_BEFORE_LOOP_1 = (flash_read_byte(FLASH_ADDR_SEND_RETURN_BEFORE_LOOP_1)==1) ? 1 : 0; //default OFF
-
+	flash_mode_SEND_RETURN_BEFORE_LOOP_0 =
+		(flash_read_byte(FLASH_ADDR_SEND_RETURN_BEFORE_LOOP_0) == 1) ? 1 : 0; //default OFF
+	flash_mode_SEND_RETURN_BEFORE_LOOP_1 =
+		(flash_read_byte(FLASH_ADDR_SEND_RETURN_BEFORE_LOOP_1) == 1) ? 1 : 0; //default OFF
 }
